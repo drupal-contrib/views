4261fb93e8723646f74d56681598824f4e623d1b
merlinofchaos <merlinofchaos>
2008-02-05 01:50:12 +0000

    Checkpoint checking. UI is getting better, but it is also getting further away from what currently runs, so when this is done a bunch of refactoring is going to have to happen.

---
+++ theme/views-view-row-node.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id: views-view-row-node.tpl.php,v 1.1 2008-02-05 01:50:13 merlinofchaos Exp $
+/**
+ * @file views-view-row-node.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php print $node; ?>
