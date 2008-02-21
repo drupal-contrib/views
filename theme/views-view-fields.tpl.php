3be1b014bdbdc341bad27b45eb7872270744b702
merlinofchaos <merlinofchaos>
2008-02-21 01:54:32 +0000

    Fix for views-view-fields.tpl.php and more exposed stuff.

--- theme/views-view-fields.tpl.php
+++ theme/views-view-fields.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: views-view-fields.tpl.php,v 1.1 2008-02-14 21:59:08 merlinofchaos Exp $
+// $Id: views-view-fields.tpl.php,v 1.2 2008-02-21 01:54:32 merlinofchaos Exp $
 /**
  * @file views-view-fields.tpl.php
  * Default simple view template to all the fields as a row.
@@ -7,8 +7,8 @@
  * @ingroup views_templates
  */
 ?>
-<?php foreach ($fields as $name => $css): ?>
-  <div class="$css">
+<?php foreach ($fields as $name => $class): ?>
+  <div class="<?php print $class ?>">
     <?php // this is indirection; $name contains the name of the variable. ?>
     <?php print $$name; ?>
   </div>
