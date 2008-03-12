2a5561ed04dc849368f21c752667ced87ad0b0dc
merlinofchaos <merlinofchaos>
2008-03-12 04:20:27 +0000

    Table style with a custom sorting implementation that mimics core

---
+++ theme/views-view-table.tpl.php
@@ -0,0 +1,32 @@
+<?php
+// $Id: views-view-table.tpl.php,v 1.1 2008-03-12 04:20:28 merlinofchaos Exp $
+/**
+ * @file views-view-table.tpl.php
+ * Template to display a view as a table.
+ *
+ * @ingroup views_templates
+ */
+?>
+<table>
+  <thead>
+    <tr>
+      <?php foreach ($header as $field => $data): ?>
+        <?php // @todo click sort styling and link ?>
+        <th class="views-field-<?php print $fields[$field]; ?>">
+          <?php print $data['label']; ?>
+        </th>
+      <?php endforeach ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($rows as $count => $row): ?>
+      <tr class="<?php print ($count % 2 == 0) ? 'even' : 'odd';?>">
+        <?php foreach ($row as $field => $data): ?>
+          <td class="views-field-<?php print $fields[$field]; ?>">
+            <?php print $data; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
