c9572f6277fc1c1ceee71f936f0bf3aeb62e06c8
merlinofchaos <merlinofchaos>
2008-03-29 00:58:35 +0000

    Forgot to cvs add these files.

---
+++ theme/views-more.tpl.php
@@ -0,0 +1,13 @@
+<?php
+// $Id: views-more.tpl.php,v 1.1 2008-03-29 00:58:35 merlinofchaos Exp $
+/**
+ * @file views-more.tpl.php
+ * Theme the more link
+ *
+ * - $more_url: the url for the more link
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<a href="<?php print $more_url ?>">View more</a>
