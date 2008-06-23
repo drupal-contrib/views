db46fc138e18a605c8c7d64499da651398b8fdf0
merlinofchaos <merlinofchaos>
2008-06-23 19:56:24 +0000

    #270892 by aclight (expanded by me): put default action and validator options in a fieldset to make the form less confusing.

--- js/dependent.js
+++ js/dependent.js
@@ -1,4 +1,4 @@
-// $Id: dependent.js,v 1.3 2008-04-23 01:58:47 merlinofchaos Exp $
+// $Id: dependent.js,v 1.4 2008-06-23 19:56:24 merlinofchaos Exp $
 /**
  * @file dependent.js
  *
@@ -71,6 +71,7 @@ Drupal.Views.dependent.autoAttach = function() {
       }
       // Add this ID
       Drupal.Views.dependent.bindings[bind_id].push(id);
+      console.log(bind_id + ': ' + id);
       // Big long if statement.
       // Drupal.settings.viewsAjax.formRelationships[id].values[bind_id] holds the possible values
 
