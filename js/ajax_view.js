b8800e7a671347b42ac23769285efcd6ab66c33b
merlinofchaos <merlinofchaos>
2009-11-30 19:38:58 +0000

    #636988 by Steve McKenzie: Wrong ajax data could be sent in rare circumstances when using AJAX paging.

--- js/ajax_view.js
+++ js/ajax_view.js
@@ -1,4 +1,4 @@
-// $Id: ajax_view.js,v 1.17 2009-03-25 00:11:25 merlinofchaos Exp $
+// $Id: ajax_view.js,v 1.19.2.1 2009-11-30 19:38:58 merlinofchaos Exp $
 
 /**
  * @file ajaxView.js
@@ -73,7 +73,7 @@ Drupal.behaviors.ViewsAjaxView = function() {
       })
       .addClass('views-processed')
       .submit(function () {
-        $('input[type=submit]', this).after('<span class="views-throbbing">&nbsp</span>');
+        $('input[type=submit], button', this).after('<span class="views-throbbing">&nbsp</span>');
         var object = this;
         $(this).ajaxSubmit({
           url: ajax_path,
@@ -107,18 +107,19 @@ Drupal.behaviors.ViewsAjaxView = function() {
           var target = this;
           $(this)
             .addClass('views-processed')
-            // Process pager, tablesort, and summary links.
-            .find('ul.pager > li > a, th.views-field a, .views-summary a')
+            // Process pager, tablesort, and attachment summary links.
+            .find('ul.pager > li > a, th.views-field a, .attachment .views-summary a')
             .each(function () {
               var viewData = {};
               // Construct an object using the settings defaults and then overriding
               // with data specific to the link.
               $.extend(
                 viewData,
-                settings,
                 Drupal.Views.parseQueryString($(this).attr('href')),
                 // Extract argument data from the URL.
-                Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path)
+                Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path),
+                // Settings must be used last to avoid sending url aliases to the server.
+                settings
               );
               $(this).click(function () {
                 $(this).addClass('views-throbbing');
