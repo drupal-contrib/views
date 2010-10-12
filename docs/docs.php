413e6a6072ee971e61f4a87be48aab0da0288ba0
merlinofchaos <merlinofchaos>
2010-10-12 21:02:36 +0000

    #386186 by dereine: Provide minimum hook_views_data_alter documentation.

--- docs/docs.php
+++ docs/docs.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: docs.php,v 1.16.2.2 2010-03-08 20:04:40 merlinofchaos Exp $
+// $Id: docs.php,v 1.16.2.3 2010-10-12 21:02:36 merlinofchaos Exp $
 /**
  * @file
  * This file contains no working PHP code; it exists to provide additional documentation
@@ -203,6 +203,36 @@ function hook_views_data() {
 
   return $data;
 }
+
+/**
+ * Alter table structure.
+ *
+ * You can add/edit/remove to existing tables defined by hook_views_data().
+ *
+ * This hook should be placed in MODULENAME.views.inc and it will be auto-loaded.
+ * This must either be in the same directory as the .module file or in a subdirectory
+ * named 'includes'.
+ *
+ * The full documentation for this hook is in the advanced help.
+ * @link http://views-help.doc.logrus.com/help/views/api-tables @endlink
+ */
+function hook_views_data_alter(&$data) {
+  // This example alters the title of the node: nid field for the admin.
+  $data['node']['nid']['title'] = t('Node-Nid');
+
+  // This example adds a example field to the users table
+  $data['users']['example_field'] = array(
+    'title' => t('Example field'),
+    'help' => t('Some examüple content that references a user'),
+    'handler' => 'hook_handlers_field_example_field',
+  );
+
+  // This example changes the handler of the node title field.
+  // In this handler you could do stuff, like preview of the node, when clicking the node title.
+
+  $data['node']['title']['handler'] = 'modulename_handlers_field_node_title';
+}
+
 
 /**
  * The full documentation for this hook is now in the advanced help.
