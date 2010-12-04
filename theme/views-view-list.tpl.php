4651cc7fbe62b76a86be898ada69d451b993f887
dereine <dereine>
2010-12-04 07:39:34 +0000

    #740686 by dereine, dagmar: Integrate the functionality of Semantic Views (though it takes a significantly diffrent form).

--- theme/views-view-list.tpl.php
+++ theme/views-view-list.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: views-view-list.tpl.php,v 1.3.6.1 2010-03-29 20:05:38 dereine Exp $
+// $Id: views-view-list.tpl.php,v 1.3.6.2 2010-12-04 07:39:35 dereine Exp $
 /**
  * @file views-view-list.tpl.php
  * Default simple view template to display a list of rows.
@@ -10,12 +10,13 @@
  */
 ?>
 <div class="item-list">
+<?php print $wrapper_prefix; ?>
   <?php if (!empty($title)) : ?>
     <h3><?php print $title; ?></h3>
   <?php endif; ?>
-  <<?php print $options['type']; ?>>
+  <?php print $list_type_prefix; ?>
     <?php foreach ($rows as $id => $row): ?>
       <li class="<?php print $classes_array[$id]; ?>"><?php print $row; ?></li>
     <?php endforeach; ?>
-  </<?php print $options['type']; ?>>
-</div>
+  <?php print $list_type_suffix; ?>
+<?php print $wrapper_suffix; ?>
