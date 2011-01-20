1dd327b44f2c8523f8e56ef0b64adc04f352400b
merlinofchaos <merlinofchaos>
2011-01-20 20:07:12 +0000

    #672864 by dboulet: cols- class incorrect on table, grid.

--- theme/views-view-grid.tpl.php
+++ theme/views-view-grid.tpl.php
@@ -1,19 +1,20 @@
 <?php
-// $Id: views-view-grid.tpl.php,v 1.3.4.3 2010-12-09 23:54:16 merlinofchaos Exp $
+// $Id: views-view-grid.tpl.php,v 1.3.4.4 2011-01-20 20:07:12 merlinofchaos Exp $
 /**
  * @file views-view-grid.tpl.php
  * Default simple view template to display a rows in a grid.
  *
  * - $rows contains a nested array of rows. Each row contains an array of
  *   columns.
- *
+ * - $class contains the class of the table.
+ * - $attributes contains other attributes for the table.
  * @ingroup views_templates
  */
 ?>
 <?php if (!empty($title)) : ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<table class="views-view-grid"<?php print $attributes; ?>>
+<table class="<?php print $class; ?>"<?php print $attributes; ?>>
   <tbody>
     <?php foreach ($rows as $row_number => $columns): ?>
       <?php
